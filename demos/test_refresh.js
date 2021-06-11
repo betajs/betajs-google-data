bf7b9078158372d11521406e3edce8fccf33b8c7
Oliver Friedmann <oliver.friedmann@gmail.com>
2021-06-10 20:23:27 -0400

    update refresh

--- demos/test_refresh.js
+++ demos/test_refresh.js
@@ -4,6 +4,7 @@ require("../node_modules/betajs-data/dist/betajs-data-noscoped.js");
 
 require("../dist/betajs-google-data.js");
 var C = require("../local-credentials.js");
+var FS = require("fs");
 
 var Google = require("googleapis");
 var google = new Google.google.auth.OAuth2(C.apiCredentials.client_id, C.apiCredentials.client_secret);
@@ -16,4 +17,8 @@ console.log(google.credentials);
 google.refreshAccessToken(function (err, creds) {
     console.log(err, creds);
     console.log(google.credentials);
+    if (!err && creds) {
+        C.userCredentials = creds;
+        FS.writeFileSync("../local-credentials.js", "module.exports = " + JSON.stringify(C, null, 2) + ";");
+    }
 });
